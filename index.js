a692dd94f52bc26f5a4ad8cebf3ae90d7ffd834b
Justin Schellenberg <jrschellenberg@gmail.com>
2018-06-24 12:00:47 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import { googleMapAPI, algorithmiaAPI } from "./secrets";
+
+const Algorithmia = require('algorithmia');
+const client = Algorithmia(algorithmiaAPI);
+const NodeGeocoder = require('node-geocoder');
+
+//let mapAPIKey = "AIzaSyCQClwazNXqYZJpilU-8L7Uow0GdtCFYrE";
+
+var options = {
+	provider: 'google',
+	
+	// Optional depending on the providers
+	httpAdapter: 'https', // Default
+	apiKey: googleMapAPI, // for Mapquest, OpenCage, Google Premier
+	formatter: null         // 'gpx', 'string', ...
+};
+
+const geocoder = NodeGeocoder(options);
+
+function callAlgo() {
+	
+	var input = {
+		"points": ["Ankara", "İzmir", "Eskişehir", "Afyonkarahisar", "Konya"],
+		"startpoint": "İstanbul",
+		"endpoint": "Istanbul"
+	};
+	client.algo("akadal/TSP/0.2.1")
+		.pipe(input)
+		.then(function (response) {
+			console.log(response.get());
+		});
+}
+
+
+function geoCode(){
+	geocoder.geocode('29 champs elysée paris')
+		.then(function(res) {
+			console.log(res);
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+}
+
+callAlgo();
+
+geoCode();
+
+
+
+
