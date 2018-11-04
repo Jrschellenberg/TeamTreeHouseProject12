0a255887da518986ee1378d6fec217678733567e
Justin Schellenberg <justin.schellenberg@boldcommerce.com>
2018-11-03 19:43:36 -0600

    upgrade babel, and tsp core functionality

--- src/utilities/algorithmUtils.js
+++ src/utilities/algorithmUtils.js
@@ -2,6 +2,11 @@ require('dotenv').config();  // Get all of our secrets...
 const algorithmia = require('algorithmia-updated');
 const client = algorithmia(process.env.ALGORITHMIA_API_KEY);
 const nodeGeocoder = require('node-geocoder');
+const distance = require('google-distance-matrix');
+
+distance.key(process.env.GOOGLE_MAP_API_KEY);
+distance.mode('driving');
+
 const options = {
 	provider: 'google',
 	
@@ -86,11 +91,178 @@ export default class AlgorithmUtils {
 	}
 	
 	
-	static calculateRoute(locations){
+	static queryDistances(data){
+		return new Promise((resolve, reject) => {
+      const {points, startpoint, endpoint} = data;
+      
+      let origins = JSON.parse(JSON.stringify(points)); //deep copy
+      origins.unshift(startpoint);
+      
+      let destinations = JSON.parse(JSON.stringify(points));
+      destinations.push(endpoint);
+			
+      distance.matrix(origins, destinations, (err, distances) => {
+        if (!err){
+        	let data = {distances: distances, origins: origins, destinations: destinations};
+          resolve(data);
+        }
+        reject(err);
+      });
+		})
+	}
+	
+	static calculateDistancesArray(distances, origins, destinations){
+
+		let possibleCombinations = [];
 		
+		for(let i = 0; i<distances.rows.length; i++){
+			for(let j = 0; j < distances.rows[i].elements.length; j++){
+				let addValue = true;
+				let value = distances.rows[i].elements[j].distance.value;
+				//console.log(`Hit Loop i = ${i} And J = ${j}`);
+				if(value !== 0){
+					for(let k=0; k < possibleCombinations.length; k++){
+						if(possibleCombinations[k].origin === destinations[j] && possibleCombinations[k].destination === origins[i]){
+							addValue = false;
+						}
+					}
+					if(addValue){
+            let obj = {};
+            obj.origin = origins[i];
+            obj.destination = destinations[j];
+            obj.distanceValue = value;
+            possibleCombinations.push(obj);
+					}
+				}
+			}
+		}
 		
-		
+		console.log(possibleCombinations);
+		return possibleCombinations;
 	}
 	
 	
+	static findShortestPath(data, dictionary) {
+		const {points, startpoint, endpoint } = data;
+		const paths = startpoint === endpoint ? 1 + points.length : 2 + points.length;
+		let currentLocation = null;
+		let suitableSelections;
+		
+		console.log(paths);
+		for(let i=0; i<paths; i++){
+			if(i === 0){ // we know we are on start point.
+				currentLocation = startpoint;
+			}	
+			suitableSelections = dictionary.filter((distancePoint) => { // Get locations 
+				return distancePoint.origin === currentLocation;
+			});
+			
+			
+		}
+	}
+}
+
+/**
+ * Get all possible paths
+ * @param {GraphVertex} startVertex
+ * @param {GraphVertex[][]} [paths]
+ * @param {GraphVertex[]} [path]
+ */
+function findAllPaths(startVertex, paths = [], path = []) {
+  // Clone path.
+  const currentPath = [...path];
+  
+  // Add startVertex to the path.
+  currentPath.push(startVertex);
+  
+  // Generate visited set from path.
+  const visitedSet = currentPath.reduce((accumulator, vertex) => {
+    const updatedAccumulator = { ...accumulator };
+    updatedAccumulator[vertex.getKey()] = vertex;
+    
+    return updatedAccumulator;
+  }, {});
+  
+  // Get all unvisited neighbors of startVertex.
+  const unvisitedNeighbors = startVertex.getNeighbors().filter((neighbor) => {
+    return !visitedSet[neighbor.getKey()];
+  });
+  
+  // If there no unvisited neighbors then treat current path as complete and save it.
+  if (!unvisitedNeighbors.length) {
+    paths.push(currentPath);
+    
+    return paths;
+  }
+  
+  // Go through all the neighbors.
+  for (let neighborIndex = 0; neighborIndex < unvisitedNeighbors.length; neighborIndex += 1) {
+    const currentUnvisitedNeighbor = unvisitedNeighbors[neighborIndex];
+    findAllPaths(currentUnvisitedNeighbor, paths, currentPath);
+  }
+  
+  return paths;
+}
+
+/**
+ * @param {number[][]} adjacencyMatrix
+ * @param {object} verticesIndices
+ * @param {GraphVertex[]} cycle
+ * @return {number}
+ */
+function getCycleWeight(adjacencyMatrix, verticesIndices, cycle) {
+  let weight = 0;
+  
+  for (let cycleIndex = 1; cycleIndex < cycle.length; cycleIndex += 1) {
+    const fromVertex = cycle[cycleIndex - 1];
+    const toVertex = cycle[cycleIndex];
+    const fromVertexIndex = verticesIndices[fromVertex.getKey()];
+    const toVertexIndex = verticesIndices[toVertex.getKey()];
+    weight += adjacencyMatrix[fromVertexIndex][toVertexIndex];
+  }
+  
+  return weight;
+}
+
+/**
+ * BRUTE FORCE approach to solve Traveling Salesman Problem.
+ *
+ * @param {Graph} graph
+ * @return {GraphVertex[]}
+ */
+export default function bfTravellingSalesman(graph) {
+  // Pick starting point from where we will traverse the graph.
+  const startVertex = graph.getAllVertices()[0];
+  
+  // BRUTE FORCE.
+  // Generate all possible paths from startVertex.
+  const allPossiblePaths = findAllPaths(startVertex);
+  
+  // Filter out paths that are not cycles.
+  const allPossibleCycles = allPossiblePaths.filter((path) => {
+    /** @var {GraphVertex} */
+    const lastVertex = path[path.length - 1];
+    const lastVertexNeighbors = lastVertex.getNeighbors();
+    
+    return lastVertexNeighbors.includes(startVertex);
+  });
+  
+  // Go through all possible cycles and pick the one with minimum overall tour weight.
+  const adjacencyMatrix = graph.getAdjacencyMatrix();
+  const verticesIndices = graph.getVerticesIndices();
+  let salesmanPath = [];
+  let salesmanPathWeight = null;
+  for (let cycleIndex = 0; cycleIndex < allPossibleCycles.length; cycleIndex += 1) {
+    const currentCycle = allPossibleCycles[cycleIndex];
+    const currentCycleWeight = getCycleWeight(adjacencyMatrix, verticesIndices, currentCycle);
+    
+    // If current cycle weight is smaller then previous ones treat current cycle as most optimal.
+    if (salesmanPathWeight === null || currentCycleWeight < salesmanPathWeight) {
+      salesmanPath = currentCycle;
+      salesmanPathWeight = currentCycleWeight;
+    }
+  }
+  
+  // Return the solution.
+  return salesmanPath;
 }
--- test/utilities.js
+++ test/utilities.js
@@ -1,16 +1,37 @@
-const server = require('../src/app');
-let dataBaseFinishSeed = false;
+//During the test the env variable is set to test
+'use strict';
+process.env.NODE_ENV = 'test';
 
-server.on('appStarted', () => {
-	dataBaseFinishSeed = true;
-});
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect;
 
-export function beforeTest(done){
-	if (dataBaseFinishSeed) {
-		done();
-	} //if it has finished seeding it will hit callback, else forever hangs
-	server.on('appStarted', () => { //Once finish seed, set to true and stop hang.
-		done();
-	});
-	
+import AlgorithmUtils from '../src/utilities/algorithmUtils';
+
+
+const validTest = {
+  points:["49.8561519,-97.16069069999998"],
+  startpoint:"49.8088807,-97.2297858",
+  endpoint:"49.8088807,-97.2297858"
+};
+
+const longerTest = {
+  points:["49.8561519,-97.16069069999998","49.932471,-97.06972489999998","50.08294370000001,-97.2263077"],
+  startpoint:"49.8088807,-97.2297858",
+  endpoint:"49.8088807,-97.2297858"
 }
+
+
+describe('POST ALGORITHMIA', () => {
+  it('should return true', (done) => {
+    AlgorithmUtils.queryDistances(longerTest).then((data) => {
+      const {distances, origins, destinations } = data;
+      const distanceDictoinary = AlgorithmUtils.calculateDistancesArray(distances, origins, destinations);
+      AlgorithmUtils.findShortestPath(longerTest, distanceDictoinary);
+      done();
+    }).catch((err) => {
+      console.log("error occured, ", err);
+    });
+  });
+});
